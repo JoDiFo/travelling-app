d92043ab158fd571069e4c841612c905d6217f32
alexandr-fokin <sasafokin2@gmail.com>
2024-10-23 18:47:56 +0300

    fix: enabled cors for server

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({origin: 'http://localhost:5173' })
   await app.listen(3000, () => {
     console.log('Server is running on port 3000');
   });
