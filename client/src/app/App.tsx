31f99a1c5ddcecd93c99e28e3b119b1ee09247b3
alexandr-fokin <sasafokin2@gmail.com>
2024-10-25 16:52:20 +0300

    feat: add auth check

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -2,11 +2,28 @@ import { router } from "@/app/router";
 import { RouterProvider } from "react-router-dom";
 
 import "@/app/styles/index.scss";
+import { useAppSelector } from "./redux";
+import { useMemo } from "react";
 
 export function App() {
+  const { error, isLoading, user } = useAppSelector((state) => state.user);
+
+  const provider = useMemo(() => {
+    return router(true);
+  }, [user]);
+
+  if (isLoading) {
+    return "Loading...";
+  }
+
+  if (error) {
+    console.log(error);
+    return "Error";
+  }
+
   return (
     <div className="app">
-      <RouterProvider router={router(false)} />
+      <RouterProvider router={provider} />
     </div>
   );
 }
