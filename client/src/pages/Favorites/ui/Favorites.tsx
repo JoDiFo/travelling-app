e82ac6fb043329972f778dc998424e3e1f1c6fc8
alexandr-fokin <sasafokin2@gmail.com>
2024-10-26 12:06:07 +0300

    feat: add display of favorites

--- client/src/pages/Favorites/ui/Favorites.tsx
+++ client/src/pages/Favorites/ui/Favorites.tsx
@@ -1,55 +1,41 @@
+import { selectUser, useAppSelector } from "@/app/redux";
 import styles from "./style.module.scss";
-import { RouteCardProps } from "@/widgets/RouteCard";
 import { CardContainer } from "@/widgets/CardContainer";
-
-const cardData: RouteCardProps[] = [
-  {
-    id: 1,
-    title: "Великолепная троица",
-    type: "Пеший",
-    region: "Минск",
-    duration: "4,5 часа",
-    guide: "Симон Кирилл",
-    cost: 24,
-    isFavorite: true,
-  },
-  {
-    id: 2,
-    title: "Великолепная троица",
-    type: "Пеший",
-    region: "Минск",
-    duration: "4,5 часа",
-    guide: "Симон Кирилл",
-    cost: 24,
-    isFavorite: true,
-  },
-  {
-    id: 3,
-    title: "Великолепная троица",
-    type: "Пеший",
-    region: "Минск",
-    duration: "4,5 часа",
-    guide: "Симон Кирилл",
-    cost: 24,
-    isFavorite: true,
-  },
-  {
-    id: 4,
-    title: "Великолепная троица",
-    type: "Пеший",
-    region: "Минск",
-    duration: "4,5 часа",
-    guide: "Симон Кирилл",
-    cost: 24,
-    isFavorite: true,
-  },
-];
+import { TravelRoute, TravelRouteService } from "@/entities/trabelRoute";
+import { UPDATE_TRAVEL_ROUTES_EVENT } from "@/shared/utils/constants";
+import { NotificationService } from "@/shared/utils/notificationService";
+import { useState, useEffect } from "react";
 
 export function Favorites() {
+  const userId = useAppSelector(selectUser)?.sub as string;
+  const [travelRoutes, setTravelRoutes] = useState<TravelRoute[]>([]);
+
+  const getTravelRoutes = async () => {
+    const favorites = await TravelRouteService.getFavorites(userId);
+    favorites.data.forEach((route) => (route.isFavorite = true));
+
+    setTravelRoutes(favorites.data);
+  };
+
+  useEffect(() => {
+    getTravelRoutes();
+  }, []);
+
+  useEffect(() => {
+    NotificationService.subscribe(UPDATE_TRAVEL_ROUTES_EVENT, getTravelRoutes);
+
+    return () => {
+      NotificationService.unsubscribe(
+        UPDATE_TRAVEL_ROUTES_EVENT,
+        getTravelRoutes
+      );
+    };
+  }, []);
+
   return (
     <main className={styles.homePage}>
       <h1>Избранные маршруты</h1>
-      <CardContainer cardData={cardData} />
+      <CardContainer cardData={travelRoutes} />
     </main>
   );
 }
