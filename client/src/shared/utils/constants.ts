3e9ad5efceeaa61c89908ce26f8a1f75ab6efaad
alexandr-fokin <sasafokin2@gmail.com>
2024-10-31 10:03:12 +0300

    refactor: move images to constants and add more images

--- client/src/shared/utils/constants.ts
+++ client/src/shared/utils/constants.ts
@@ -1,3 +1,14 @@
+import Amsterdam from "../assets/Amsterdam.png";
+import Bruge from "../assets/Bruge.png";
+import Edinburgh from "../assets/Edinburgh.png";
+import Florance from "../assets/Florance.png";
+import Grodno from "../assets/Grodno.png";
+import Minsk from "../assets/Minsk.png";
+import Prague from "../assets/Prague.png";
+import Stockgholm from "../assets/Stockgholm.png";
+import Vilnus from "../assets/Vilnus.png";
+import Warsaw from "../assets/Warsaw.png";
+
 export const PAGE_ROUTES = {
   TRAVEL_ROUTES: "/routes",
   CREATE_ROUTE: "/routes/create",
@@ -12,28 +23,53 @@ export const PAGE_ROUTES = {
 export const IMAGE_MAP = [
   {
     id: "1",
-    imageUrl: "/Warsaw.png",
+    imageUrl: Warsaw,
     title: "Королевский путь Варшавы",
   },
   {
     id: "2",
-    imageUrl: "/Bruge.png",
+    imageUrl: Bruge,
     title: "Легенды Брюгге",
   },
   {
     id: "3",
-    imageUrl: "/Amsterdam.png",
+    imageUrl: Amsterdam,
     title: "Гармония Гродно",
   },
   {
     id: "4",
-    imageUrl: "/Edinburgh.png",
+    imageUrl: Edinburgh,
     title: "Амстердамская романтика",
   },
   {
     id: "5",
-    imageUrl: "/Grodno.png",
+    imageUrl: Grodno,
     title: "Эдинбург: от замка до королевского сада",
+  },
+  {
+    id: "6",
+    imageUrl: Florance,
+    title: "Солнечная Флоренция",
+  },
+  {
+    id: "7",
+    imageUrl: Minsk,
+    title: "Сердце Минска: архитектура и современность",
+  },
+  {
+    id: "8",
+    imageUrl: Prague,
+    title: "Прага сквозь века",
+  },
+  {
+    id: "9",
+    imageUrl: Stockgholm,
+    title: "Скандинавская одиссея в Стокгольме",
+  },
+  {
+    id: "10",
+    imageUrl: Vilnus,
+    title: "Кофейная Вильнюсская променад",
   },
 ];
 
